445a0be11e334ae04f2a8611c4b060a95595cca9
Igor Uvarov <kuflash@ya.ru>
2022-07-05 20:43:00 +0300

    add solution for m1-5.1

---
+++ module-1/lesson-5/5.1.hs
@@ -0,0 +1,13 @@
+inc n = n + 1
+
+double n = n * 2
+
+square n = n ^ 2
+
+ifEven f x = if even x then f x else x
+
+ifEvenInc = ifEven inc
+
+ifEvenDouble = ifEven double
+
+ifEvenSquare = ifEven square
