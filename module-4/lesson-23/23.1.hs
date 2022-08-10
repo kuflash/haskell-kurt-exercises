c758a10cdab95364c68af9118e6d59f0652d4c9e
Igor Uvarov <kuflash@ya.ru>
2022-08-10 18:23:31 +0300

    add solution for m4-23.1

---
+++ module-4/lesson-23/23.1.hs
@@ -0,0 +1,14 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+import qualified Data.Text as T
+import qualified Data.Text.IO as TIO
+
+helloPerson :: T.Text -> T.Text
+helloPerson name = mconcat ["Hello, ", name, "!"]
+
+main :: IO ()
+main = do
+  TIO.putStrLn "Hello! What is your name?"
+  name <- TIO.getLine
+  let statement = helloPerson name
+  TIO.putStrLn statement
