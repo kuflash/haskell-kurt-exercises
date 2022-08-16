6c9aecfa2b3cf41bfa449b97e78b3033053c76c0
Igor Uvarov <kuflash@ya.ru>
2022-08-16 19:01:04 +0300

    add solution for m5-31.2

---
+++ module-5/lesson-31/31.2.hs
@@ -0,0 +1,34 @@
+import qualified Data.Map as Map
+
+type Pizza = (Double, Double)
+
+areaGivenDiameter :: Double -> Double
+areaGivenDiameter size = pi * (size / 2) ^ 2
+
+costPerCm :: Pizza -> Double
+costPerCm (size, cost) = cost / areaGivenDiameter size
+
+comparePizzas :: Pizza -> Pizza -> Pizza
+comparePizzas p1 p2 =
+  if costPerCm p1 < costPerCm p2
+    then p1
+    else p2
+
+describePizza :: Pizza -> String
+describePizza (size, cost) = "Pizza with size " ++ show size ++ " is cheaper at " ++ show (costPerCm (size, cost))
+
+costData :: Map.Map Int Double
+costData = Map.fromList [(1, 150), (2, 220)]
+
+sizeData :: Map.Map Int Double
+sizeData = Map.fromList [(1, 30), (2, 50)]
+
+pizzas :: [Pizza]
+pizzas = [(30, 150), (50, 220), (40, 210), (45, 180)]
+
+listMain :: [String]
+listMain = do
+  pizza1 <- pizzas
+  pizza2 <- pizzas
+  let betterPizza = comparePizzas pizza1 pizza2
+  return (describePizza betterPizza)
