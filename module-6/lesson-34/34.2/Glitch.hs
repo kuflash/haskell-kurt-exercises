b04b94ccd1c194f5f790fef0f5b2dc3e42374fe1
Igor Uvarov <kuflash@ya.ru>
2022-08-18 20:33:49 +0300

    add solution for m6-34.2

---
+++ module-6/lesson-34/34.2/Glitch.hs
@@ -0,0 +1,54 @@
+module Glitch (randomReplaceByte, randomSortSection, randomReverseSection) where
+
+import qualified Data.ByteString.Char8 as BC
+import System.Random
+
+intToChar :: Int -> Char
+intToChar int = toEnum safeInt
+  where
+    safeInt = int `mod` 255
+
+intToBC :: Int -> BC.ByteString
+intToBC int = BC.pack [intToChar int]
+
+replaceByte :: Int -> Int -> BC.ByteString -> BC.ByteString
+replaceByte loc charValue bytes = mconcat [before, newChar, after]
+  where
+    (before, rest) = BC.splitAt loc bytes
+    after = BC.drop 1 rest
+    newChar = intToBC charValue
+
+sortSection :: Int -> Int -> BC.ByteString -> BC.ByteString
+sortSection start size bytes = mconcat [before, changed, after]
+  where
+    (before, rest) = BC.splitAt start bytes
+    (target, after) = BC.splitAt size rest
+    changed = BC.reverse (BC.sort target)
+
+reverseSection :: Int -> Int -> BC.ByteString -> BC.ByteString
+reverseSection start size bytes = mconcat [before, changed, after]
+  where
+    (before, rest) = BC.splitAt start bytes
+    (target, after) = BC.splitAt size rest
+    changed = BC.reverse target
+
+randomReplaceByte :: BC.ByteString -> IO BC.ByteString
+randomReplaceByte bytes = do
+  let bytesLength = BC.length bytes
+  location <- randomRIO (1, bytesLength)
+  charValue <- randomRIO (0, 255)
+  return (replaceByte location charValue bytes)
+
+randomSortSection :: BC.ByteString -> IO BC.ByteString
+randomSortSection bytes = do
+  let sectionSize = 50000
+  let bytesLength = BC.length bytes
+  start <- randomRIO (0, bytesLength - sectionSize)
+  return (sortSection start sectionSize bytes)
+
+randomReverseSection :: BC.ByteString -> IO BC.ByteString
+randomReverseSection bytes = do
+  let bytesLength = BC.length bytes
+  sectionSize <- randomRIO (bytesLength `div` 2, bytesLength)
+  start <- randomRIO (0, bytesLength - sectionSize)
+  return (reverseSection start sectionSize bytes)
